b5864d90c70af6a752f158278f3f8ae289cd3b40
Sebastien Lambla <seb@serialseb.com>
2010-05-17 23:57:20 +0100

    aggregate repository

---
+++ src/OpenWrap/Repositories/AggregatePackageRepository.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using OpenRasta.Wrap.Dependencies;
+using OpenRasta.Wrap.Sources;
+
+namespace OpenWrap.Repositories
+{
+    public class AggregatePackageRepository : IPackageRepository
+    {
+        readonly IPackageRepository _localRepository;
+        readonly IPackageRepository _userRepository;
+        readonly IEnumerable<IPackageRepository> _remoteRepositories;
+
+        public AggregatePackageRepository(IPackageRepository localRepository, IPackageRepository userRepository, IEnumerable<IPackageRepository> remoteRepositories)
+        {
+            _localRepository = localRepository;
+            _userRepository = userRepository;
+            _remoteRepositories = remoteRepositories;
+            UnifyPackagesByName();
+        }
+
+        void UnifyPackagesByName()
+        {
+            PackagesByName = _localRepository.PackagesByName.SelectMany(x=>x).Concat(_userRepository.PackagesByName.SelectMany(i=>i)).ToLookup(x => x.Name);
+        }
+
+        public ILookup<string, IPackageInfo> PackagesByName { get; private set; }
+
+        public IPackageInfo Find(WrapDependency dependency)
+        {
+            return _localRepository.Find(dependency)
+                   ?? CopyFromUserToLocalRepository(dependency)
+                      ?? LoadFromRemoteRepository(dependency);
+            
+        }
+
+        IPackageInfo LoadFromRemoteRepository(WrapDependency dependency)
+        {
+            return null;
+        }
+
+        IPackageInfo CopyFromUserToLocalRepository(WrapDependency dependency)
+        {
+            return null;
+        }
+    }
+}
