2a7a2c06e73d9fa908d1f19098a2cd37bf9b2007
Andrew Davey <andrew@equin.co.uk>
2010-06-02 20:55:47 +0100

    Add basic CamelToSpaceName implementation and tests

--- src/OpenWrap/StringExtensions.cs
+++ src/OpenWrap/StringExtensions.cs
@@ -1,11 +1,13 @@
+using System.Text.RegularExpressions;
 namespace OpenWrap
 {
     public static class StringExtensions
     {
         public static string CamelToSpacedName(this string str)
         {
-            //TODO: Evil not implemented yet.
-            return str;
+            return caseChange.Replace(str, "$1 $2");
         }
+
+        static readonly Regex caseChange = new Regex("([a-z])([A-Z])");
     }
 }
