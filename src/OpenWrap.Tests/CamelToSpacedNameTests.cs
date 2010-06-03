169e042901c78427d9d54b01f2cab76f5b570087
Andrew Davey <andrew@equin.co.uk>
2010-06-03 17:32:41 +0100

    Add camel case test

---
+++ src/OpenWrap.Tests/CamelToSpacedNameTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace OpenWrap.Tests
+{
+    [TestFixture]
+    public class CamelToSpacedNameTests
+    {
+        [Test]
+        [Sequential]
+        public void Test(
+            [Values("HelloWorld",  "ABC", "CustomerID",  "SomeLongerText")] string input,
+            [Values("Hello World", "ABC", "Customer ID", "Some Longer Text")] string output)
+        {
+            Assert.AreEqual(output, input.CamelToSpacedName());
+        }
+    }
+}
