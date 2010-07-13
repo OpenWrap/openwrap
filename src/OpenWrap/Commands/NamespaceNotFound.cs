baace588b72d7b06527daf399747d1d9fb7fe986
Andrew Davey <andrew@equin.co.uk>
2010-07-13 20:32:36 +0100

    Fix issue: NamespaceNotFound doesn't display friendly error message

--- src/OpenWrap/Commands/NamespaceNotFound.cs
+++ src/OpenWrap/Commands/NamespaceNotFound.cs
@@ -1,19 +1,29 @@
 using System.Collections.Generic;
+using System.Linq;
 
 namespace OpenWrap.Commands
 {
     public class NamespaceNotFound : Error
     {
-        readonly IEnumerable<string> _namespaceList;
+        readonly string _message;
 
         public NamespaceNotFound(IEnumerable<string> namespaceList)
         {
-            _namespaceList = namespaceList;
+            var names = namespaceList.ToArray();
+            if (names.Length == 0)
+                _message = "Namespace not found.";
+            else
+                _message = "Ambiguous namespace. Possible matches: " + string.Join(", ", names);
         }
 
         public bool Success
         {
             get { return false; }
+        }
+
+        public override string ToString()
+        {
+            return _message;
         }
     }
 }
