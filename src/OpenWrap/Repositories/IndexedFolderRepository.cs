d4c5aaba518329478aa480636371d7dc67fb7fc7
Sebastien Lambla <sebastien.lambla@credit-suisse.com>
2010-09-07 15:16:36 +0100

    Added tests for indexed folder repository

---
+++ src/OpenWrap/Repositories/IndexedFolderRepository.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Xml.Linq;
+using OpenWrap.Dependencies;
+
+namespace OpenWrap.Repositories
+{
+    public class IndexedFolderRepository : IPackageRepository
+    {
+        public IndexedFolderRepository(string path)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ILookup<string, IPackageInfo> PackagesByName
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public IPackageInfo Find(WrapDependency dependency)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IPackageInfo Publish(string packageFileName, Stream packageStream)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool CanPublish
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public string Name
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public XDocument IndexFile { get; set; }
+    }
+}
