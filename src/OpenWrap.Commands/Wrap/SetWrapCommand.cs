986a353923dd3be2c6ae5b4eda8d82cd3ec9afde
Andrew Davey <andrew@equin.co.uk>
2010-10-18 21:12:15 +0100

    Add more test cases

--- src/OpenWrap.Commands/Wrap/SetWrapCommand.cs
+++ src/OpenWrap.Commands/Wrap/SetWrapCommand.cs
@@ -1,9 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using OpenWrap.Dependencies;
 using OpenFileSystem.IO;
+using OpenWrap.Dependencies;
 
 namespace OpenWrap.Commands.Wrap
 {
--- src/OpenWrap.Tests/Commands/Wrap/setWrapCommand.cs
+++ src/OpenWrap.Tests/Commands/Wrap/setWrapCommand.cs
@@ -1,10 +1,11 @@
 ﻿using System;
 using System.Linq;
 using NUnit.Framework;
+using OpenWrap.Commands;
 using OpenWrap.Commands.Wrap;
+using OpenWrap.Dependencies;
 using OpenWrap.Repositories;
 using OpenWrap.Testing;
-using OpenWrap.Dependencies;
 
 namespace OpenWrap.Tests.Commands.Wrap
 {
@@ -112,6 +113,72 @@ namespace OpenWrap.Tests.Commands.Wrap
         {
             var vertices = Environment.Descriptor.Dependencies.First().VersionVertices;
             vertices.First().ShouldBeOfType<AnyVersionVertex>();
+        }
+    }
+
+    class set_wrap_minversion_2 : context.command_context<SetWrapCommand>
+    {
+        public set_wrap_minversion_2()
+        {
+            given_dependency("depends: sauron = 1.0.0");
+            given_project_package("sauron", new Version("1.0.0.0"));
+
+            when_executing_command("sauron", "-minversion", "2.0.0");
+        }
+
+        [Test]
+        public void dependency_version_is_greaterthan_2()
+        {
+            var vertex = Environment.Descriptor.Dependencies.First().VersionVertices.First();
+            var greater = vertex as GreaterThanVersionVertex;
+            greater.Version.ShouldBe(new Version("2.0.0"));
+        }
+    }
+
+
+    class set_wrap_minversion_2_maxversion_3 : context.command_context<SetWrapCommand>
+    {
+        public set_wrap_minversion_2_maxversion_3()
+        {
+            given_dependency("depends: sauron = 1.0.0");
+            given_project_package("sauron", new Version("1.0.0.0"));
+
+            when_executing_command("sauron", "-minversion", "2.0.0", "-maxversion", "3.0.0");
+
+            vertices = Environment.Descriptor.Dependencies.First().VersionVertices.ToArray();
+        }
+
+        VersionVertex[] vertices;
+
+        [Test]
+        public void vertex_0_is_greaterthan_2()
+        {
+            var greater = vertices[0] as GreaterThanVersionVertex;
+            greater.Version.ShouldBe(new Version("2.0.0"));
+        }
+
+        [Test]
+        public void vertex_1_is_lessthan_3()
+        {
+            var lessThan = vertices[1] as LessThanVersionVertex;
+            lessThan.Version.ShouldBe(new Version("3.0.0"));
+        }
+    }
+
+    class set_wrap_conflicting_version_inputs : context.command_context<SetWrapCommand>
+    {
+        public set_wrap_conflicting_version_inputs()
+        {
+            given_dependency("depends: sauron = 1.0.0");
+            given_project_package("sauron", new Version("1.0.0.0"));
+
+            when_executing_command("sauron", "-version", "2.0.0", "-maxversion", "3.0.0");
+        }
+
+        [Test]
+        public void error_returned()
+        {
+            Results.First().ShouldBeOfType<Error>();
         }
     }
 }
