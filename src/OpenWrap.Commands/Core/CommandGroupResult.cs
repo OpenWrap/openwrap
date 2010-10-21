d814c6173acaa2e7d32cdcbd087782e4904776e9
Andrew Davey <andrew@equin.co.uk>
2010-10-21 16:39:50 +0100

    Group and sort the get-help command list. Closes Issue 83.

---
+++ src/OpenWrap.Commands/Core/CommandGroupResult.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace OpenWrap.Commands.Core
+{
+    public class CommandGroupResult : Success
+    {
+        readonly string _noun;
+        readonly ICommandDescriptor[] _commands;
+
+        public CommandGroupResult(string noun, IEnumerable<ICommandDescriptor> commands)
+        {
+            _noun = noun;
+            _commands = commands.OrderBy(c => c.Verb).ToArray();
+        }
+
+        public override string ToString()
+        {
+            var builder = new StringBuilder();
+            builder.AppendLine(_noun);
+            foreach (var command in _commands)
+            {
+                builder.Append("  ").Append(command.Verb).Append(": ").AppendLine(command.Description);
+            }
+            return builder.ToString();
+        }
+    }
+}
