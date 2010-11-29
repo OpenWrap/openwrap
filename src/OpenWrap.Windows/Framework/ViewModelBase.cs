5cd523c3d74aabe844986565f8443b66abc1a873
Anthony Steele <Anthony@PlasticAvalon.org>
2010-11-29 22:15:41 +0000

    refactor to UI: moved classes to seperate files, added ViewModelBase.cs

---
+++ src/OpenWrap.Windows/Framework/ViewModelBase.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.ComponentModel;
+using System.Linq.Expressions;
+
+namespace OpenWrap.Windows.Framework
+{
+    public abstract class ViewModelBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void RaisePropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+        protected void RaisePropertyChanged<T>(Expression<Func<T, object>> propertyLambda)
+        {
+            if (PropertyChanged != null)
+            {
+                string propertyName = ExtractPropertyNameFromExpresssion(propertyLambda);
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+        private static string ExtractPropertyNameFromExpresssion<T>(Expression<Func<T, object>> propertyLambda)
+        {
+            if (propertyLambda.NodeType != ExpressionType.Lambda)
+            {
+                throw new ArgumentException("The expression must be a lambda");
+            }
+
+            MemberExpression funcBody = propertyLambda.Body as MemberExpression;
+            if (funcBody == null)
+            {
+                throw new ArgumentException("the method body must be a member expression");                    
+            }
+
+            return funcBody.Member.Name;
+        }
+    }
+}
--- src/OpenWrap.Windows/MainViewModel.cs
+++ src/OpenWrap.Windows/MainViewModel.cs
@@ -1,127 +1,64 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Collections.ObjectModel;
-using System.ComponentModel;
 using System.Linq;
-using System.Text;
-using OpenWrap;
 using OpenWrap.Commands;
-using OpenWrap.Dependencies;
-using OpenWrap.Repositories;
-using OpenWrap.Services;
+using OpenWrap.Windows.Framework;
 
 namespace OpenWrap.Windows
 {
-    public class MainViewModel : INotifyPropertyChanged
+    public class MainViewModel : ViewModelBase
     {
+        private IEnumerable<NounSlice> _nouns;
+        private NounSlice _selectedNoun;
+
         public MainViewModel()
         {
             var commands = Services.Services.GetService<ICommandRepository>();
-            Nouns = commands != null 
-                ? commands.GroupBy(x => x.Noun).Select(x => CreateNounSlice(x)).ToList()
-                : MockCommands();
+            Nouns = commands != null ? RealCommands(commands) : MockCommands();
         }
 
-        NounSlice CreateNounSlice(IGrouping<string, ICommandDescriptor> x)
+        public IEnumerable<NounSlice> Nouns
+        {
+            get
+            {
+                return _nouns;
+            }
+            set
+            {
+                _nouns = value;
+                RaisePropertyChanged<MainViewModel>(o => o.Nouns);
+            }
+        }
+
+        public NounSlice SelectedNoun
+        {
+            get
+            {
+                return _selectedNoun;
+            }
+            set
+            {
+                _selectedNoun = value;
+                RaisePropertyChanged<MainViewModel>(o => o.SelectedNoun);
+            }
+        }
+        
+        private static NounSlice CreateNounSlice(IGrouping<string, ICommandDescriptor> x)
         {
             if (x.Key.Equals("wrap", StringComparison.OrdinalIgnoreCase))
                 return new WrapSlice(x.Key, x.Select(y => new VerbSlice(y)));
-            return new NounSlice(x.Key,x.Select(y=>new VerbSlice(y)));
+            return new NounSlice(x.Key, x.Select(y => new VerbSlice(y)));
         }
 
-        IEnumerable<NounSlice> MockCommands()
+        private static IEnumerable<NounSlice> RealCommands(ICommandRepository commands)
+        {
+            return commands.GroupBy(x => x.Noun).Select(x => CreateNounSlice(x));
+        }
+        
+        private static IEnumerable<NounSlice> MockCommands()
         {
             yield return new NounSlice("Test 1", new[] { new VerbSlice(new InMemoryCommandDescriptor()) });
             yield return new NounSlice("Test 2", new[] { new VerbSlice(new InMemoryCommandDescriptor()) });
         }
-
-        IEnumerable<NounSlice> _nouns;
-        public IEnumerable<NounSlice> Nouns
-        {
-            get { return _nouns; }
-            set { _nouns = value; PropertyChanged(this, new PropertyChangedEventArgs("Nouns")); }
-        }
-
-        NounSlice _selectedNoun;
-        public NounSlice SelectedNoun
-        {
-            get { return _selectedNoun; }
-            set { _selectedNoun = value; PropertyChanged(this, new PropertyChangedEventArgs("SelectedNoun")); }
-        }
-
-        public event PropertyChangedEventHandler PropertyChanged = (s, e) => { };
     }
-
-    internal class InMemoryCommandDescriptor : ICommandDescriptor
-    {
-        public string Noun { get; private set; }
-        public string Verb { get; private set; }
-        public string DisplayName { get; private set; }
-        public string Description { get; private set; }
-        public IDictionary<string, ICommandInputDescriptor> Inputs { get; private set; }
-        public ICommand Create()
-        {
-            return null;
-        }
-    }
-
-    public class NounSlice : INotifyPropertyChanged
-    {
-        public NounSlice(string noun, IEnumerable<VerbSlice> commandDescriptors)
-        {
-            Noun = noun;
-            Commands = commandDescriptors;
-        }
-
-        public IEnumerable<VerbSlice> Commands { get; set; }
-
-        public string Noun { get; set; }
-        public event PropertyChangedEventHandler PropertyChanged = (s,e)=>{};
-        protected virtual void NotifyPropertyChanged(string propertyName) { PropertyChanged(this, new PropertyChangedEventArgs(propertyName));}
-    }
-    
-    public class WrapSlice : NounSlice
-    {
-        protected IEnvironment Environment { get { return Services.Services.GetService<IEnvironment>(); } }
-        protected IPackageResolver PackageResolver { get { return Services.Services.GetService<IPackageResolver>(); } }
-
-        DependencyResolutionResult _projectDependencies;
-        public DependencyResolutionResult ProjectDependencies
-        {
-            get { return _projectDependencies; }
-            set { _projectDependencies = value; NotifyPropertyChanged("ProjectDependencies"); }
-        }
-
-        //public IEnumerable<SystemPackage> SystemDependencies { get { return Environment.SystemRepository.PackagesByName.Select(x => new SystemPackage(x.Key, x)); } }
-        public WrapSlice(string noun, IEnumerable<VerbSlice> commandDescriptors) : base(noun, commandDescriptors)
-        {
-
-            if (Environment != null && Environment.ProjectRepository != null)
-            {
-                ProjectDependencies = PackageResolver.TryResolveDependencies(Environment.Descriptor, new[] { Environment.ProjectRepository });
-            }
-        }
-
-    }
-
-    public class LocalDependencyViewModel
-    {
-    }
-
-    public class VerbSlice
-    {
-        public VerbSlice(ICommandDescriptor commandDescriptor)
-        {
-            Verb = commandDescriptor.Verb;
-            this.Command = commandDescriptor;
-        }
-
-        public ICommandDescriptor Command { get; set; }
-
-        public string Verb { get; set; }
-    }
-    public class DependenciesViewModel
-    {
-    }
-
 }
