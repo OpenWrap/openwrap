56e55dcfa406ec6f2bf2003dcc5529281a18e442
AnthonySteele <Anthony@PlasticAvalon.org>
2010-12-30 22:49:46 +0000

    Start main window and viewnmodel manually in app.xaml

--- src/OpenWrap.Windows/App.xaml.cs
+++ src/OpenWrap.Windows/App.xaml.cs
@@ -1,11 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Linq;
 using System.Windows;
 using OpenFileSystem.IO;
 using OpenFileSystem.IO.FileSystems.Local;
-using OpenWrap.Build;
 using OpenWrap.Commands;
 using OpenWrap.Configuration;
 using OpenWrap.PackageManagement;
@@ -13,46 +9,53 @@ using OpenWrap.PackageManagement.AssemblyResolvers;
 using OpenWrap.PackageManagement.DependencyResolvers;
 using OpenWrap.PackageManagement.Deployers;
 using OpenWrap.PackageManagement.Exporters;
-using OpenWrap.Repositories;
 using OpenWrap.Runtime;
-using OpenWrap.Services;
 using OpenWrap.Tasks;
 
 namespace OpenWrap.Windows
 {
     /// <summary>
-    /// Interaction logic for App.xaml
+    /// Interaction logic for applicatiiion
     /// </summary>
     public partial class App : Application
     {
-        public App()
-        {
-            //Preloader.PreloadDependencies("openfilesystem", "sharpziplib");
-        }
         protected override void OnStartup(StartupEventArgs e)
         {
+            RegisterServices();
+            ShowMainWindow();
+            base.OnStartup(e);
+        }
 
-            Services.Services.RegisterService<RuntimeAssemblyResolver>(new RuntimeAssemblyResolver());
+        private static void RegisterServices()
+        {
+            Services.Services.RegisterService(new RuntimeAssemblyResolver());
             Services.Services.TryRegisterService<IFileSystem>(() => LocalFileSystem.Instance);
             Services.Services.TryRegisterService<IConfigurationManager>(() => new DefaultConfigurationManager(Services.Services.GetService<IFileSystem>().GetDirectory(DefaultInstallationPaths.ConfigurationDirectory)));
             Services.Services.TryRegisterService<IEnvironment>(() => new CurrentDirectoryEnvironment());
-
 
             Services.Services.TryRegisterService<IPackageResolver>(() => new ExhaustiveResolver());
             Services.Services.TryRegisterService<IPackageExporter>(() => new DefaultPackageExporter());
             Services.Services.TryRegisterService<IPackageDeployer>(() => new DefaultPackageDeployer());
             Services.Services.TryRegisterService<IPackageManager>(() => new DefaultPackageManager(
                 Services.Services.GetService<IPackageDeployer>(),
-                Services.Services.GetService<IPackageResolver>()
-                ));
-
+                Services.Services.GetService<IPackageResolver>()));
 
             Services.Services.RegisterService<ITaskManager>(new TaskManager());
 
             var repo = new CommandRepository(Services.Services.GetService<IEnvironment>().Commands());
 
             Services.Services.TryRegisterService<ICommandRepository>(() => repo);
-            base.OnStartup(e);
+        }
+
+        private void ShowMainWindow()
+        {
+            MainViewModel viewModel = new MainViewModel();
+
+            Main mainWindow = new Main();
+            mainWindow.DataContext = viewModel;
+
+            MainWindow = mainWindow;
+            mainWindow.Show();
         }
     }
 }
