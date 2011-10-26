70964314ed27241f546744f6ed168500c1b2a781
Sebastien Lambla <seb@serialseb.com>
2011-10-26 17:28:25 +0200

    ignores system commands if they can't be loaded

--- src/OpenWrap/Commands/EnvironmentExtensions.cs
+++ src/OpenWrap/Commands/EnvironmentExtensions.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using OpenWrap.Collections;
 using OpenWrap.PackageManagement;
 using OpenWrap.PackageManagement.Exporters;
+using OpenWrap.Repositories;
 using OpenWrap.Runtime;
 
 namespace OpenWrap.Commands
@@ -15,13 +16,26 @@ namespace OpenWrap.Commands
             var packageExporter = Services.Services.GetService<IPackageExporter>();
             if (packageExporter == null)
                 throw new InvalidOperationException("A package exporter service hasn't been found.");
+            IEnumerable<ICommandDescriptor> commands = environment.ProjectRepository == null
+                                                               ? Enumerable.Empty<ICommandDescriptor>()
+                                                               : GetCommandsFor(environment, packageExporter, environment.ProjectRepository);
+            return commands.Concat(GetCommandsFor(environment, packageExporter, environment.SystemRepository)).ToList();
+        }
 
-            return packageExporter
-                    .GetExports<IExport>("commands", environment.ExecutionEnvironment, new[] { environment.ProjectRepository, environment.SystemRepository }.NotNull())
-                    .SelectMany(x => x.Items)
-                    .OfType<ICommandExportItem>()
-                    .Select(x => x.Descriptor)
-                    .ToList();
+        static IEnumerable<ICommandDescriptor> GetCommandsFor(IEnvironment environment, IPackageExporter packageExporter, IPackageRepository repository)
+        {
+            try
+            {
+                return packageExporter
+                        .GetExports<IExport>("commands", environment.ExecutionEnvironment, new[] { repository, environment.SystemRepository }.NotNull())
+                        .SelectMany(x => x.Items)
+                        .OfType<ICommandExportItem>()
+                        .Select(x => x.Descriptor)
+                        .ToList();
+            }catch
+            {
+                return Enumerable.Empty<ICommandDescriptor>();
+            }
         }
     }
 }
