a5e893f5ff5c6d1bbf13aeba97cc29500bd2249a
Chris Eldredge <celdredge@fool.com>
2012-01-05 18:35:48 -0500

    Treat build output messages as string literals, not as message format patterns.

--- src/OpenWrap.Commands/Wrap/BuildWrapCommand.cs
+++ src/OpenWrap.Commands/Wrap/BuildWrapCommand.cs
@@ -405,7 +405,9 @@ namespace OpenWrap.Commands.Wrap
             foreach (var t in packageBuilders.SelectMany(x => x.Build()))
             {
                 if (t is InfoBuildResult || (t is TextBuildResult && !Quiet))
-                    yield return new Info(t.Message);
+                {
+                    yield return new Info("{0}", t.Message);
+                }
                 else if (t is FileBuildResult)
                 {
                     var buildResult = (FileBuildResult)t;
@@ -416,7 +418,7 @@ namespace OpenWrap.Commands.Wrap
                 }
                 else if (t is ErrorBuildResult)
                 {
-                    yield return new Error(t.Message);
+                    yield return new Error("{0}", t.Message);
                     yield break;
                 }
             }
